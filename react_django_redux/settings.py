00e5805fc12075f24016483be7771aed61dcae10
Abdou Abarchi Aboubacar <57944650+AbdouAbarchiAboubacar@users.noreply.github.com>
2020-08-29 17:17:33 +0100

    apply change

--- react_django_redux/settings.py
+++ react_django_redux/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'y5lku=$yxcr&gmd&34el4ie!h(1ug2f3(*h%^qmzs!$$(yltpl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #'djangoreact-redux.herokuapp.com'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoreact-redux.herokuapp.com']
 
 # Application definition
 
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'leads',
     'accounts',
     'rest_framework',
@@ -126,8 +127,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'frontend/static/frontend'),
 )
