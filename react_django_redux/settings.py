ec265f01c27098b4fa7c0c8cb9e42001b8fe3250
Abdou Abarchi Aboubacar <57944650+AbdouAbarchiAboubacar@users.noreply.github.com>
2020-08-29 01:42:24 +0100

    apply change

--- react_django_redux/settings.py
+++ react_django_redux/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -136,7 +135,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
 
-
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
